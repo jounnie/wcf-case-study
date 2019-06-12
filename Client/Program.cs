b214a691f16520d7ac1cebde78edab2c40274af6
Jonathan Ruiz <jonathan@ruiz.io>
2019-06-12 23:28:21 +0200

    cosmetics

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.ServiceModel;
-using DemoSqlServer;
 using WCFandEFService;
 
-
-namespace ServiceClient
+namespace Client
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
